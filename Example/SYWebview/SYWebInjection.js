9261ef45ecffae49805155c2f9842d0565a8c88c
燕雪峰 <ssi-yanxf@dfmc.com.cn>
2021-06-23 14:50:33 +0800

    调试

--- Example/SYWebview/SYWebInjection.js
+++ Example/SYWebview/SYWebInjection.js
@@ -7,8 +7,19 @@ function SYWebInjection() {
     obj.nativeSendToH5 = nativeSendToH5;
     obj.nativeCallback = nativeCallback;
     obj.handleNativeMsg = handleNativeMsg;
+    obj.syRealH5SendToNative = syRealH5SendToNative;
     return obj;
 }
+
+function SYWebMsg(msgid,key,params,issuccess){
+    var obj = new Object();
+    obj.msgid = msgid;
+    obj.key = key;
+    obj.params = params;
+    obj.issuccess = Boolean(issuccess);
+    return obj;
+}
+
 //保存h5发送消息到native的回调方法
 var sy_web_func_dic = {};
 //保存native发送消息到h5的回调方法
@@ -16,43 +27,49 @@ var sy_native_func_dic = {};
 var uniqueId = 1;
 
 function logInNative(params) {
-    var newParams = { param: params, key: "sy_web_log" };
-    this.h5SendToNative(newParams);
+    var msgid = this.createMsgId()
+    this.syRealH5SendToNative(msgid,"sy_web_log",params);
+}
+
+function h5SendToNative(key,params,success,fail) {
+    var msgid = this.createMsgId()
+    this.syRealH5SendToNative(msgid,key,params,true,success,fail);
 }
 
 //h5发送消息给native
-function h5SendToNative(params, success, fail) {
+function syRealH5SendToNative(msgid,key,params,isSuccess, success, fail) {
     /*
-    params = {key:"xxx",param:param}
-    */
-    var callbackId = this.createMsgId();
-    var newParams = {
-        callbackId: callbackId,
-        params: params
+ msg = {
+ sy_msgid:xxx,
+ sy_key:xxx,
+ sy_params:xxx,
+ sy_success:bool
+ }
+ */
+    var msg = {
+        sy_msgid: msgid,
+        sy_key: key,
+        sy_params:params,
+        sy_success:isSuccess
     };
-    sy_web_func_dic[callbackId] = { success: success, fail: fail };
-    window.webkit.messageHandlers.msgFromH5.postMessage(newParams);
+    sy_web_func_dic[msgid] = { success: success, fail: fail };
+    window.webkit.messageHandlers.syMsgFromH5.postMessage(msg);
 }
 
 ///native执行完消息之后的回调
-function syWebcallback(params) {
-    //native传过来的参数都是字符串，需要先转换成对象
-    var jsonObj = JSON.parse(params);
-    var msgid = jsonObj["callbackId"];
+function syWebcallback(msgid,params,issuccess) {
     var callback = sy_web_func_dic[msgid];
     if (callback == undefined) { return; }
     sy_web_func_dic[msgid] = null;
-    var param = jsonObj["params"];
-    var isSuc = Boolean(jsonObj["isSucCallback"]);
-    if (isSuc) {
+    if (issuccess) {
         var success = callback["success"];
         if (success == undefined) { return; }
-        success(param);
+        success(params);
         return;
     }
     var fail = callback["fail"];
     if (fail == undefined) { return; }
-    fail(param);
+    fail(params);
 }
 
 function createMsgId() {
@@ -64,8 +81,14 @@ function createMsgId() {
 function nativeSendToH5(params) {
     //native传过来的参数都是字符串，需要先转换成对象
     var jsonObj = JSON.parse(params);
-    var msgid = jsonObj["callbackId"];
-    var param = jsonObj["params"];
+    var msgid = jsonObj["sy_msgid"];
+    var param = jsonObj["sy_params"];
+    var key = jsonObj["sy_key"];
+    var issuccess = Boolean(jsonObj["sy_success"]);
+    if (key == "sy_web_callback"){
+        this.syWebcallback(msgid,params,issuccess);
+        return
+    }
     var sucCallback = function (sucParams) {
         this.nativeCallback(sucParams, msgid, true);
     };
@@ -75,22 +98,15 @@ function nativeSendToH5(params) {
     if (this.handleNativeMsg == undefined) {
         return;
     }
-    this.handleNativeMsg(param, sucCallback, failCallback);
+    this.handleNativeMsg(key,param, sucCallback, failCallback);
 }
 
 function nativeCallback(params, msgid, issuccess) {
-    var newParams = {
-        key: "sy_nativeCallback",
-        param: params,
-        msgId: msgid,
-        issuccess: issuccess
-    };
-    this.h5SendToNative(newParams);
+    this.syRealH5SendToNative(msgid,"sy_nativeCallback",params,issuccess);
 }
 
 //需要重写此方法
-function handleNativeMsg(params, success, fail) {
-    var key = params["key"];
+function handleNativeMsg(key,params, success, fail) {
     if(key == "test"){
         this.logInNative(params);
         // success({nice:"success"});
